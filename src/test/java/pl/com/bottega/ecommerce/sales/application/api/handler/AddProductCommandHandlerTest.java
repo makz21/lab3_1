95515fd21012f9d00b2961484a9074979f028d5a
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-05-06 20:32:11 +0200

    1.2 created and initialized AddProductCommandHandlerTest

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,51 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import org.junit.Before;
+import org.mockito.Mock;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.Client;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.system.application.SystemContext;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+public class AddProductCommandHandlerTest {
+
+    private AddProductCommandHandler addProductCommandHandler;
+    private AddProductCommand command;
+    @Mock
+    private ReservationRepository reservationRepository;
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private SuggestionService suggestionService;
+    @Mock
+    private ClientRepository clientRepository;
+    @Mock
+    private SystemContext systemContext;
+    @Mock
+    private Reservation reservation;
+    @Mock
+    private Product product;
+
+    @Before
+    public void init(){
+        command = new AddProductCommand(Id.generate(), new Id("1"), 1);
+        Client client = new Client();
+        when(product.isAvailable()).thenReturn(true);
+        when(reservationRepository.load(any())).thenReturn(reservation);
+        when(productRepository.load(any())).thenReturn(product);
+        when(suggestionService.suggestEquivalent(product, client)).thenReturn(product);
+        addProductCommandHandler = new AddProductCommandHandler(reservationRepository, productRepository,
+                suggestionService, clientRepository, systemContext);
+    }
+
+}
